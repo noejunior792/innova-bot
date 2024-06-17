ab97aa915f0938d3f9b9c6b6f149b5d50bb983ac
Noé Júnior <noedombaxe@gmail.com>
2024-06-17 09:20:09 +0100

    feat: add src

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const venom = require("venom-bot")
+const venom = require("venom-bot");
+const axios = require("axios");
+const banco = require("./src/banco");
 
 venom.create({
     session : "innovatech",
@@ -7,8 +9,40 @@ venom.create({
 .then((client) => start(client))
 .catch((error) => console.log(error));
 
+const header = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer $OPENAI_API_KEY"
+}
+
 const start = (client) => {
-    client.onMessage((message) => (
-        console.log(message)
-    ))
+    client.onMessage((message) => {
+        const userCadastrado = banco.db.find(numero => numero.num === message.from);
+        if(!userCadastrado){
+            console.log("Cadastrando usuario");
+            banco.db.push({num: message.from, historico : [ ]});
+        }
+        else{
+            console.log("Usuário já cadastrado");
+        }
+
+        const historico = banco.db.find(num => num.num === message.from);
+        historico.historico.push("user: " + message.body)
+
+        axios.post("https://api.openai.com/v1/chat/completions",{
+            "model":"gpt-3.5-turbo",
+            "messages":[
+                {"role":"system", "content":"historico de conversas " + historico.historico},
+                {"role":"user","content":message.body}]
+        },{
+            headers: header
+        })
+        .then((response)=>{
+            console.log(response.data.choices[0].content);
+            historico.historico.push("assistent: " +  response.data.choices[0].content);
+            client.sendText(message.from, response.data.choices[0].content)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    })
 }
