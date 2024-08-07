d46d662b35f6310e34de9f843051d86353c39355
Noé Júnior <noedombaxe@gmail.com>
2024-08-07 15:19:26 +0100

    ⚙️chore: add .env in projet for openai key

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const venom = require("venom-bot");
 const axios = require("axios");
 const banco = require("./src/banco");
+require('dotenv').config();
 
-const treinamento = `
-
-`
+const treinamento = ``;
 
 venom.create({
-    session : "innovatech",
+    session: "innovatech",
     multidevice: true
 })
 .then((client) => start(client))
@@ -15,39 +14,38 @@ venom.create({
 
 const header = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer $OPENAI_API_KEY"
-}
+    "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+};
 
 const start = (client) => {
     client.onMessage((message) => {
         const userCadastrado = banco.db.find(numero => numero.num === message.from);
-        if(!userCadastrado){
+        if (!userCadastrado) {
             console.log("Cadastrando usuário");
-            banco.db.push({num: message.from, historico : [ ]});
-        }
-        else{
+            banco.db.push({ num: message.from, historico: [] });
+        } else {
             console.log("Usuário já cadastrado");
         }
 
         const historico = banco.db.find(num => num.num === message.from);
-        historico.historico.push("user: " + message.body)
+        historico.historico.push("user: " + message.body);
 
-        axios.post("https://api.openai.com/v1/chat/completions",{
-            "model":"gpt-3.5-turbo",
-            "messages":[
-                {"role":"system", "content":treinamento},
-                {"role":"system", "content":"historico de conversas " + historico.historico},
-                {"role":"user","content":message.body}]
-        },{
+        axios.post("https://api.openai.com/v1/chat/completions", {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                { "role": "system", "content": treinamento },
+                { "role": "system", "content": "historico de conversas " + historico.historico },
+                { "role": "user", "content": message.body }]
+        }, {
             headers: header
         })
-        .then((response)=>{
+        .then((response) => {
             console.log(response.data.choices[0].message.content);
-            historico.historico.push("assistent: " +  response.data.choices[0].content);
-            client.sendText(message.from, response.data.choices[0].content)
+            historico.historico.push("assistent: " + response.data.choices[0].content);
+            client.sendText(message.from, response.data.choices[0].content);
         })
-        .catch((error)=>{
-            console.log(error)
-        })
-    })
+        .catch((error) => {
+            console.log(error);
+        });
+    });
 }
